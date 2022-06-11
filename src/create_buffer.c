bde3c40c4935bf42afb17fbdce8d21c0b84b6301
Matthew Wells <secret>
2022-06-10 20:27:45 -0500

    more ncurses fun, got coloured text

--- src/create_buffer.c
+++ src/create_buffer.c
@@ -4,17 +4,37 @@
 #include <string.h>
 
 int rand_char();
+int rand_color_pair();
 void fill_print_buffer(char*, struct winsize);
 
 
+
 int main(){
+    initscr();
+    start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(2, COLOR_BLUE, COLOR_BLACK);
+    init_pair(3, COLOR_CYAN, COLOR_BLACK);
+    init_pair(4, COLOR_GREEN, COLOR_BLACK);
+    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(6, COLOR_RED, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE, COLOR_BLACK);
+    init_pair(0, COLOR_BLACK, COLOR_BLACK);
+    
+    
     srand(time(0));
     struct winsize ws = get_window_size();
     char* term_window = malloc((ws.ws_col * ws.ws_row) * sizeof(term_window));
 
     fill_print_buffer(term_window, ws);
 
-    initscr(); // initialize screen
+    // defining color pairs
+    if(has_colors() == FALSE){
+        printf("Terminal does not have color support exiting\n");
+        endwin();
+        exit(1);
+    }
+
     mvprintw(0, 0, term_window); // mv cursor to start and prin screen
     fill_print_buffer(term_window, ws); // randomly populate the buffer
     getch(); // wait for key press
@@ -23,6 +43,19 @@ int main(){
     mvprintw(0, 0, term_window); // move back to start and print screen
    // clear();
     //printw(term_window);
+    refresh();
+    getch();
+    clear();
+    move(0,0); // move to position to add the char
+    fill_print_buffer(term_window, ws);
+    for(size_t i = 0; i < (ws.ws_col * ws.ws_row); ++i){
+        //need to set a colour attribute eachtime something is written
+        //attron(COLOR_PAIR(1));
+        addch(term_window[i] | COLOR_PAIR(rand_color_pair()));
+       // attroff(COLOR_PAIR(1)); // good idea to restore terminal state each time
+    }
+    
+
     refresh();
     getch();
     endwin();
@@ -41,9 +74,14 @@ void fill_print_buffer(char* char_buff, struct winsize ws){
     char_buff[ws.ws_col * ws.ws_row] = '\0';
 }
 
+int rand_color_pair(){
+    int r = rand() % 7;
+    return r;
+
+}
 
 int rand_char(){
     int r = rand(); 
-    int ret_char = r % (26);
+    int ret_char = r % 26;
     return ret_char + 'A'; // offset the alphabet to starting position
 }
