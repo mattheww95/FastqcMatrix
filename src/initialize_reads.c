1ae20fe811e82e7bc0ef1d72bbaac12558567476
Matthew Wells <mattwells9@shaw.ca>
2022-06-12 13:59:02 -0500

    Added test fastq data generated from wgsim

--- src/initialize_reads.c
+++ src/initialize_reads.c
@@ -10,34 +10,52 @@ Need to hold the color pair definitions in here too...
 
 need to make a jumptable base on ranges, or switch may be best
 */
+
 #include <ncurses.h>
 #include <stdint.h>
 
 
 // for new illumina only
 // have 42 values to fill in
+
+//---Define the colour pair values
 #define POOR_QUALITY 1
 #define MODERATE_QUALITY 2
 #define GOOD_QUALITY 3
 
+
+//--Define cut offs for quality
+#define GOOD_QUALITY_SCORE '5'
+#define MODERATE_QUALTIY_SCORE '.'
+// No need to do a poor one as it wil defualt
+
+
+
 uint8_t return_colour_pair(char basq){
-    char base_num = baseq - '!'; // corresponds to 33 or 0 in encoding
+    //char base_num = baseq - '!'; // corresponds to 33 or 0 in encoding
     //higher is better
-    switch (base_num)
+    
+    uint8_t ret_val;
+    switch (base_q)
     {
-    case base_num > 13:
-        return GOOD_QUALITY;
+    case base_num > GOOD_QUALITY_SCORE;
+        ret_val = GOOD_QUALITY;
+        break;
+    case base_num > MODERATE_QUALITY_SCORE;
+        ret_val = MODERATE_QUALITY;
         break;
     
     default:
+        ret_val = POOR_QUALITY;
         break;
     }
-
-};
+    return ret_val;
+}
 
 typedef struct base_qc
 {
     char* called_base;
     char* qulait_value;
+    uint8_t color_pair_val;
 
 }base_qc;
