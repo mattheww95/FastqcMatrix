f72f5bd64cfb7bd3c63b322e791d7a74ea84ad30
Matthew Wells <mattwells9@shaw.ca>
2022-08-05 19:55:39 -0500

    Restarted can write this much cleaner

--- src/fastq_parser.c
+++ src/fastq_parser.c
@@ -1,168 +1,127 @@
 /*
-Parse a fastq file, 
+ * Re-writing alot of the program now that I am wiser with C
+ * */
 
-format is:
-    header
-    sequence
-    +
-    quality string
-
-
-
-need to make sure not loading entire file into memory at once
-going to make this a module for loading and preparing fastq data
-*/
-
+#include "klib/kseq.h"
+#include "klib/kstring.h"
+#include "string.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> // need to wrap this in ifndef for gnu vs posix
-#include <stdbool.h>
-#include <stdint.h>
-#include <initialize_reads.h> 
+#include <zlib.h>
 
-#define LINE_SIZE 256
-#define FASTQ_LOADS 1000
+KSEQ_INIT(gzFile, gzread);
+static uint64_t sequence_count = 0;
+void kmemcpy(kseq_t *, kseq_t *);
 
-typedef struct fastq_nucleotide{
-    char nucleotide;
-    char quality_value;
-    uint8_t color_pair;
-}fastq_nucleotide;
-
-typedef struct fastq_nucleotides{
-    fastq_nucleotide** data;
-    uint32_t counter;
-}fastq_nucleotides;
-
-typedef struct fastq_line{
-    char* header;
-    char* sequence;
-    char* quality_string;
-    struct fastq_line* next;
-}fastq_line;
-
-
-
-void init_fastq_data(FILE* some_file_ptr, fastq_line* node1){
-    // iniitialize the fastq data struct
-    char line[LINE_SIZE];
-    int p_qual_time = 0;
-
-    node1->header = NULL;
-    node1->sequence = NULL;
-    node1->quality_string = NULL;
-    node1->next = NULL;    
-    fastq_line** tracer;// = malloc(sizeof(tracer));
-    tracer = &node1;
-
-    while(fgets(line, LINE_SIZE, some_file_ptr)) {
-
-        if(line[0] == '@'){
-            (*tracer)->header = malloc(sizeof(*(*tracer)->header) * strlen(line));
-            strcpy((*tracer)->header, line);
-
-        }else if((*tracer)->header != NULL && (*tracer)->sequence == NULL){
-            (*tracer)->sequence = malloc(sizeof((*tracer)->sequence) * strlen(line));
-            strcpy((*tracer)->sequence, line);
-            //printf("Sequence: %s\n", (*tracer)->sequence);
-
-        }else if((*tracer)->header != NULL && (*tracer)->sequence != NULL && p_qual_time == 0){
-            p_qual_time = 1;
-            
-        }else if(p_qual_time == 1){
-            (*tracer)->quality_string = malloc(sizeof(*(*tracer)->quality_string) * strlen(line));
-            strcpy((*tracer)->quality_string, line);
-            p_qual_time = 0;
-            (*tracer)->next = malloc(sizeof((*tracer)->next));
-            tracer = &(*tracer)->next;
-            (*tracer)->header = NULL;            
-            (*tracer)->sequence = NULL;            
-            (*tracer)->next = NULL;            
-            (*tracer)->quality_string = NULL;            
-        }
+/*
+ *@brief Read in the fastq file and prepare struct of fastq data
+ *
+ *@param inpath The path to the fastq file to open
+ * */
+kseq_t **read_file(const char *inpath) {
+  gzFile fp = NULL;
+  kseq_t *seq = NULL;
+  int l = 0;
+  uint64_t start_size = 40000;
+  fp = gzopen(inpath, "r");
+  seq = kseq_init(fp);
+  // Initialize enought memory for 40000 sequences
+  kseq_t **fastq_data = malloc(sizeof(fastq_data) * start_size);
+  while ((l = kseq_read(seq)) >= 0) {
+    if (start_size == sequence_count) {
+      fastq_data = realloc(fastq_data, start_size * 3);
+      start_size = start_size * 3;
     }
-    
+    kseq_t *sequence = malloc(sizeof(*sequence));
+    // sequence = seq;
+    kmemcpy(sequence, seq);
+    printf("Test: %s\n", seq->name.s);
+    fastq_data[sequence_count] = sequence;
+    // printf("%s: %s\n", fastq_data[sequence_count]->name.s,
+    //        fastq_data[sequence_count]->seq.s);
+    sequence_count++;
+  }
+  kseq_destroy(seq);
+  return fastq_data;
 }
 
+/*@brief Create a copy of a kseq object
+ *
+ *@param dest The kseq_t object to be copy into
+ *@param src The seq object the data is being copied from
+ *
+ */
+void kmemcpy(kseq_t *dest, kseq_t *src) {
+  // Copy the comment
+  dest->comment.l = src->comment.l;
+  dest->comment.m = src->comment.m;
+  dest->comment.s = malloc(sizeof(char) * src->comment.l);
+  memcpy(dest->comment.s, src->comment.s, src->comment.l);
 
-fastq_nucleotide* reads_to_fastq(fastq_line* fastq_data_){
-    // Create an array of arrays holding struct info for each sequence
-    // this can definately be done earlier...
-    // make a struct array
-    fastq_nucleotide* seq_data = malloc(sizeof(fastq_nucleotide) * strlen(fastq_data_->quality_string));
-    //memset(seq_data, 0, strlen(fastq_data_->quality_string) * sizeof(fastq_nucleotide*));
-    for(size_t i = 0; i < strlen(fastq_data_->sequence); ++i){ 
-        seq_data[i].nucleotide = fastq_data_->sequence[i];
-        seq_data[i].quality_value = fastq_data_->quality_string[i];
-        seq_data[i].color_pair = color_pair_val(&fastq_data_->quality_string[i]);
-    }
-    return seq_data;
+  // copy name
+  dest->name.l = src->name.l;
+  dest->name.m = src->name.m;
+  dest->name.s = malloc(sizeof(char) * src->name.l);
+  memcpy(dest->name.s, src->name.s, src->name.l);
 
+  // copy the sequence
+  dest->seq.l = src->seq.l;
+  dest->seq.m = src->seq.m;
+  dest->seq.s = malloc(sizeof(char) * src->seq.l);
+  memcpy(dest->seq.s, src->seq.s, src->seq.l);
+
+  // copy the quality string
+  dest->qual.l = src->qual.l;
+  dest->qual.m = src->qual.m;
+  dest->qual.s = malloc(sizeof(char) * src->qual.l);
+  memcpy(dest->qual.s, src->qual.s, src->qual.l);
+
+  dest->last_char = src->last_char;
 }
 
-void destroy_fastq_term(fastq_nucleotides data){
-    // Free the memory used to display the data to the terminal
-    for(size_t i = 0; i < data.counter;++i){
-        free(data.data[i]);
-    }
-    
+/*
+ *@brief Free memory used by the reads
+ *
+ */
+void destroy_reads(kseq_t **read_data) {
+  for (size_t i = 0; i < sequence_count; i++) {
+    // printf("%s: %s\n", read_data[i]->name.s, read_data[i]->seq.s);
+    kseq_destroy(read_data[i]);
+  }
+  free(read_data);
 }
 
-void destroy_fastq_line(fastq_line* entry){
-    free(entry->header);
-    free(entry->quality_string);
-    free(entry->sequence);
+int main(int argc, char **argv) {
+  if (argc == 1) {
+    fprintf(stderr, "Could not open fastq file\n");
+    return -1;
+  }
+  kseq_t **data = read_file(argv[1]);
+  destroy_reads(data);
 }
 
-
-void load_fastq(char* filename){
-    // Load a fastq file and return the initialize a fastq data struct
-
-   FILE *fptr;
-   fptr = fopen(filename, "r");
-   if(fptr == NULL){
-       fprintf(stderr, "Could not open fastq file\n");
-       exit(-1);
-   }
-   fastq_line* node = malloc(sizeof(*node));
-   fastq_line** fastq_data = NULL;
-   fastq_data = &node;
-   init_fastq_data(fptr, node); // pasing in double pointer as to not have dangling pointer or stale stack frame
-   if(fastq_data == NULL){
-       fprintf(stderr, "No data returned\n");
-       exit(-1);
-   }
-
-   fastq_nucleotides fastq_term;
-   fastq_term.counter = 0;
-   fastq_term.data  = malloc(sizeof(fastq_nucleotide*) * FASTQ_LOADS); // malloc enough for 500 lines while playing:
-   
-
-   fastq_line* entry;
-    // this is to destruct the linked list, will probaby move it into its own method
-   while((*fastq_data)->next != NULL){
-       //printf("Header: %s \n", (*fastq_data)->header);
-       //printf("Sequence: %s \n", (*fastq_data)->sequence);
-       //printf("Quality Data: %s \n", (*fastq_data)->quality_string);
-       fastq_term.data[fastq_term.counter] = reads_to_fastq(*fastq_data);
-       fastq_term.counter += 1;
-
-       entry = *fastq_data;
-       fastq_data = &(*fastq_data)->next;
-       //fprintf(stderr, "Freeing entry: %s\n", entry->header);
-       destroy_fastq_line(entry);
-   }
-   destroy_fastq_term(fastq_term);
-   //free(*fastq_data);
-   fclose(fptr);
-}
-
-
-
-
-
-
-int main(int argc, char** argv){
-    load_fastq(argv[1]);
-}
-
+// int main(int argc, char **argv) {
+//   gzFile fp = NULL;
+//   kseq_t *seq = NULL;
+//   int l = 0;
+//   if (argc == 1) {
+//     fprintf(stderr, "Usage: %s <in.fasta>\n", argv[0]);
+//     return 1;
+//   }
+//   fp = gzopen(argv[1], "r");
+//   seq = kseq_init(fp);
+//   while ((l = kseq_read(seq)) >= 0) {
+//     printf("name: %s\n", seq->name.s);
+//     printf("length: %lu\n", seq->seq.l);
+//     if (seq->comment.l)
+//       printf("comment: %s\n", seq->comment.s);
+//     printf("seq: %s\n", seq->seq.s);
+//     if (seq->qual.l)
+//       printf("qual: %s\n", seq->qual.s);
+//   }
+//   printf("return value: %d\n", l);
+//   kseq_destroy(seq);
+//   gzclose(fp);
+//   return 0;
+// }
