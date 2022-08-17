52584bf5ba13da50692b09268cc5580fae2bb706
Matthew Wells <mattwells9@shaw.ca>
2022-08-16 20:01:42 -0500

    removed old folders

--- src/old/initialize_reads.h
+++
@@ -1,5 +0,0 @@
-#include <ncurses.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-uint8_t color_pair_val(char baseq);
--- src/old/window_size.h
+++
@@ -1,9 +0,0 @@
-#include <sys/ioctl.h>
-#include <err.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-
-struct winsize get_window_size();
